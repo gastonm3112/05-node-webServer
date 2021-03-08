9c7856fff1dffbe8363fdd72785d4d51eacec089
gastonm3112 <gastonm3112@gmail.com>
2021-03-08 20:41:12 -0300

    Diferencias entre http de node y express

---
+++ index-old.js
@@ -0,0 +1,18 @@
+// const http = require('http');
+
+
+// http.createServer((req, res) => {
+
+//     console.log('Hola Mundo');
+
+//     res.writeHead(200, { 'Content-Type': 'application/json' });
+
+//     const user = {
+//         id: 1,
+//         name: 'Gastón'
+//     }
+
+//     res.write(JSON.stringify(user));
+//     res.end();
+
+// }).listen(3000);
