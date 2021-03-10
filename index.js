62cbd5f88e5363bd47ec46a252c08cd497945355
gastonm3112 <gastonm3112@gmail.com>
2021-03-10 19:29:12 -0300

    PORT env agregado

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
     res.send('Hello World')
 })
+
+app.get('/usuarios', (req, res) => {
+
+    res.send(('Usuarios: Gastón'));
+})
+
+
 
 app.listen(port, () => {
     console.log(`Server started - Port ${port}`);
